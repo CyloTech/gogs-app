e134022b9d2c6e1a2cf7caf3e14a1eb1999de12f
Glow <gavin@cylo.io>
2019-02-26 18:22:23 +0000

    Fixed ports on upgrade.

--- scripts/Entrypoint.sh
+++ scripts/Entrypoint.sh
@@ -6,7 +6,10 @@ if [ ! -f /etc/gogs_installed ]; then
 
     cd /home/git/gogs
 
-    mkdir -p /home/git/gogs/custom/conf
+    if [ ! -f /home/git/gogs/custom/conf/app.ini ]; then
+    #The config doesnt exist, so it must be a fresh install.
+
+        mkdir -p /home/git/gogs/custom/conf
 
 cat <<EOT >> /home/git/gogs/custom/conf/app.ini
 RUN_USER = git
@@ -61,8 +64,8 @@ INSTALL_LOCK = true
 SECRET_KEY   = ${SECRET_KEY}
 EOT
 
-    chown -R git:git /home/git
-    mkdir -p /var/log/gogs/
+        chown -R git:git /home/git
+        mkdir -p /var/log/gogs/
 
 cat << EOF >> /etc/supervisor/conf.d/gogs.conf
 [program:gogs]
@@ -83,17 +86,28 @@ user = git
 environment = HOME="/home/git", USER="git"
 EOF
 
-    # Allow git user to use port 80.
-    setcap cap_net_bind_service=+ep /home/git/gogs/gogs
+        # Allow git user to use port 80.
+        setcap cap_net_bind_service=+ep /home/git/gogs/gogs
 
-    cd /home/git/gogs
-    exec su -c "./gogs web" -s /bin/sh git &
-    sleep 10
-    exec su -c "./gogs admin create-user --name ${ADMIN_USER} --password ${ADMIN_PASS} --admin --email ${ADMIN_EMAIL}" -s /bin/sh git &
-    sleep 10
-    pkill -9 gogs
+        cd /home/git/gogs
+        exec su -c "./gogs web" -s /bin/sh git &
+        sleep 10
+        exec su -c "./gogs admin create-user --name ${ADMIN_USER} --password ${ADMIN_PASS} --admin --email ${ADMIN_EMAIL}" -s /bin/sh git &
+        sleep 10
+        pkill -9 gogs
 
-    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/$INSTANCE_ID"
+    else
+        echo "This is an update..."
+        # Put the new ports in /home/git/gogs/custom/conf/app.ini somehow.
+        sed -ie 's/HTTP_PORT        = .*/HTTP_PORT        = '${HTTP_PORT}'/g' /znc-data/configs/znc.conf
+        sed -ie 's/SSH_PORT        = .*/SSH_PORT        = '${HTTP_PORT}'/g' /znc-data/configs/znc.conf
+    fi
+
+until [[ $(curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/${INSTANCE_ID}" | grep '200') ]]
+    do
+    sleep 5
+done
+
     touch /etc/gogs_installed
 fi
 # Start supervisord and services
